69b7ae464242457435fb445983afaa885b0f755f
lucifer <lucifer.xiao@magic-beans.cn>
2016-05-23 21:23:47 +0800

    修改

--- demo/WebRoot/admin/js/ip.js
+++
@@ -1,3 +0,0 @@
-var timeout = 300;
-var ipAddress = "http://192.168.0.119:8080/aplus";
-
--- demo/WebRoot/admin/js/skin.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by wc on 2015/5/21.
- * Ä£°åÆ¤·ôJson
- *
- */
-
-
-var templeatSkin = {'topBarBgColor':'#fff000','fontColor':'#333333','logoPath':'login.png'};
